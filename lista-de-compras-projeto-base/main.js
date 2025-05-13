c8eb920b49dd6b12196998269db52c94b9b93c5b
Suzana <su_zana-santos@hotmail.com>
2025-05-12 22:08:34 -0300

    Iteração com usuário

--- lista-de-compras-projeto-base/main.js
+++ lista-de-compras-projeto-base/main.js
@@ -2,10 +2,12 @@ let listaDeItens = []
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault() //impedir padrão
     salvarItem()
+    mostrarItem()
 })
 
 function salvarItem() {
@@ -21,4 +23,30 @@ function salvarItem() {
 }
 
     console.log(listaDeItens)
+}
+
+function mostrarItem(){
+    ulItens.innerHTML = ''
+
+    listaDeItens.forEach((elemento,index) => {
+        ulItens.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+    `
+    })
+
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach(i => {
+        i.addEventListener('click',(evento) => {
+            console.log("fui clicado")
+        })
+    })
 }
