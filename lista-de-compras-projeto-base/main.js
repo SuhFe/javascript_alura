5856d0ecd334c5e459c9b6155b9d71fcc13cb4c5
Suzana <su_zana-santos@hotmail.com>
2025-05-22 10:04:17 -0300

    editando Objetos

--- lista-de-compras-projeto-base/main.js
+++ lista-de-compras-projeto-base/main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
@@ -53,6 +54,7 @@ function mostrarItem(){
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+            <button onClick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i><button><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -66,10 +68,35 @@ function mostrarItem(){
         i.addEventListener('click', (evento) => {
             valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            console.log(listaDeItens[valorDoElemento].checar)
+            mostrarItem()
         })
     })
 
+    const deletarObjetos = document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento,1)
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        })
+    })
 }
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    console.log(itemEditado.value)
+}
+
 
    
